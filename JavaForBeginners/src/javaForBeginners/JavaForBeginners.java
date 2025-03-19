075f8872aa6284e88d0ada53c3422cf84f6bd3bf
user <mixalhstzoumas@gmail.com>
2025-03-19 16:44:00 +0200

    initial commit

---
+++ JavaForBeginners/src/javaForBeginners/JavaForBeginners.java
@@ -0,0 +1,23 @@
+package javaForBeginners;
+
+
+import java.util.Scanner;
+
+public class JavaForBeginners {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");               //Τυπώνει Hello World
+		Scanner in = new Scanner(System.in);			 // Παίρνει δεδομένα απο το πληκτρολόγιο και τα βάζει στην μεταβλητη S
+		/*	Scanner s = new Scanner(System.in);  ----->  //2ος τρόπος να δουλέψει το Scanner()*/
+		String s=in.nextLine();							 //Παίρνει δεδομένα απο το πληκτρολόγιο και τα βάζει στην μεταβλητη S Τύπου String
+	    System.out.println(s+" Michael");                //Τυπώνει το περιεχόμενο της s (δλδ οτι πληρκτρολογήσαμε)
+
+	    
+	    
+	    
+	    
+	    
+	    in.close();                                    //κλείνω το Scanner για να μη καταναλώνει τζαμπα μνήμη
+	}
+
+}
