cfef8ca31f9616ec25f7da1f52941cde1e8aff17
Tzoymas Michael <mixalhstzoumas@gmail.com>
2025-03-20 15:54:52 +0200

    Update JavaForBeginners.java

--- JavaForBeginners/src/javaForBeginners/JavaForBeginners.java
+++ JavaForBeginners/src/javaForBeginners/JavaForBeginners.java
@@ -10,7 +10,7 @@ public class JavaForBeginners {
 		Scanner in = new Scanner(System.in);		    // Παίρνει δεδομένα απο το πληκτρολόγιο και τα βάζει στην μεταβλητη S
 		/*	Scanner s = new Scanner(System.in);  -----> //2ος τρόπος να δουλέψει το Scanner()*/
 		String s=in.nextLine();				    //Παίρνει δεδομένα απο το πληκτρολόγιο και τα βάζει στην μεταβλητη S Τύπου String
-	    System.out.println(s+" Michael");                       //Τυπώνει το περιεχόμενο της s (δλδ οτι πληρκτρολογήσαμε)
+	        System.out.println(s+" Michael");                       //Τυπώνει το περιεχόμενο της s (δλδ οτι πληρκτρολογήσαμε)
 
 	    
 	    
